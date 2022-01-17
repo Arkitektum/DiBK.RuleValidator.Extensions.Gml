7fa857a593d64d54da72495fb0607adfe12f1971
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2022-01-17 14:11:23 +0100

    La til hjelpemetoder.

---
+++ DiBK.RuleValidator.Extensions.Gml/Constants/GmlGeometry.cs
@@ -0,0 +1,28 @@
+﻿namespace DiBK.RuleValidator.Extensions.Gml
+{
+    public static class GmlGeometry
+    {
+        public const string CompositeCurve = "CompositeCurve";
+        public const string CompositeSolid = "CompositeSolid";
+        public const string CompositeSurface = "CompositeSurface";
+        public const string Curve = "Curve";
+        public const string GeometricComplex = "GeometricComplex";
+        public const string Grid = "Grid";
+        public const string LineString = "LineString";
+        public const string MultiCurve = "MultiCurve";
+        public const string MultiGeometry = "MultiGeometry";
+        public const string MultiPoint = "MultiPoint";
+        public const string MultiSolid = "MultiSolid";
+        public const string MultiSurface = "MultiSurface";
+        public const string OrientableCurve = "OrientableCurve";
+        public const string OrientableSurface = "OrientableSurface";
+        public const string Point = "Point";
+        public const string Polygon = "Polygon";
+        public const string PolyhedralSurface = "PolyhedralSurface";
+        public const string RectifiedGrid = "RectifiedGrid";
+        public const string Solid = "Solid";
+        public const string Surface = "Surface";
+        public const string Tin = "Tin";
+        public const string TriangulatedSurface = "TriangulatedSurface";
+    }
+}
--- DiBK.RuleValidator.Extensions.Gml/Models/GmlDocument.cs
+++ DiBK.RuleValidator.Extensions.Gml/Models/GmlDocument.cs
@@ -9,6 +9,7 @@ namespace DiBK.RuleValidator.Extensions.Gml
     public class GmlDocument : ValidationDataElement, IDisposable
     {
         private List<XElement> _features;
+        private ILookup<string, XElement> _geometryElements;
         private readonly Dictionary<string, IndexedGeometry> _geometryIndex = new(25000);
         private readonly object geoLock = new();
         private bool _disposed = false;
@@ -19,7 +20,7 @@ namespace DiBK.RuleValidator.Extensions.Gml
 
         public GmlDocument(XDocument document, string fileName, object dataType) : base(document, fileName, dataType)
         {
-            LoadFeatures(document);
+            Initialize(document);
         }
 
         public List<XElement> GetFeatures(params string[] featureNames)
@@ -32,6 +33,20 @@ namespace DiBK.RuleValidator.Extensions.Gml
                 .ToList();
         }
 
+        public List<XElement> GetFeatureGeometryElements(params string[] geometryNames)
+        {
+            if (!geometryNames.Any())
+                return _geometryElements.SelectMany(element => element).ToList();
+
+            var geometryElements = new List<XElement>();
+
+            foreach (var geometryName in geometryNames)
+                if (_geometryElements.Contains(geometryName))
+                    geometryElements.AddRange(_geometryElements[geometryName]);
+
+            return geometryElements;
+        }
+
         public Geometry GetOrCreateGeometry(XElement geoElement, out string errorMessage)
         {
             lock (geoLock)
@@ -40,7 +55,7 @@ namespace DiBK.RuleValidator.Extensions.Gml
             }
         }
 
-        private void LoadFeatures(XDocument document)
+        private void Initialize(XDocument document)
         {
             var localName = document.Root.Elements()
                 .Any(element => element.Name.LocalName == "featureMember") ? "featureMember" : "featureMembers";
@@ -49,6 +64,10 @@ namespace DiBK.RuleValidator.Extensions.Gml
                 .Where(element => element.Name.LocalName == localName)
                 .SelectMany(element => element.Elements())
                 .ToList();
+
+            _geometryElements = _features
+                .SelectMany(GmlHelper.GetFeatureGeometryElements)
+                .ToLookup(element => element.Name.LocalName);
         }
 
         public void Dispose()
